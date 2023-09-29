fc550edec5aaf758dde448bd8514f2c95f020ca9
kayan <kayanvalente25@gmail.com>
2023-09-29 10:05:32 -0300

    criação do middleware para rotas autenticadas

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+require("dotenv");
+const secret = process.env.JWT_TOKEN;
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+//verifica se o token é valido
+const withAuth = (req, res, next) => {
+  const token = req.headers["token"];
+  if (!token) {
+    res.status(400).json({ error: "acesso não autorizado" });
+  } else {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ error: "token invalido!" });
+      } else {
+        User.findOne({ email: decoded.email }).then((user) => {
+          req.user = user;
+          next();
+        });
+      }
+    });
+  }
+};
+
+module.exports = withAuth;
