6a1c2568aa90fe60f9304900c68c85003ca0441d
kayan <kayanvalente25@gmail.com>
2023-10-02 11:23:26 -0300

    instalação do cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ require("./config/database");
 const userRoutes = require("./src/routes/user");
 const noteRoutes = require("./src/routes/note");
 const Note = require("./src/models/note");
+const cors = require("cors");
 
 //CONFIGS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
